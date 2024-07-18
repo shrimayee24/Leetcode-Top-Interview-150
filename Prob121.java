104b1385d9d590752fa03fee143d579f5032fbe4
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-07-18 10:12:24 +0530

    Create Prob121.java

---
+++ Prob121.java
@@ -0,0 +1,26 @@
+class Prob121 {
+    public int maxProfit(int[] prices) 
+    {
+        //two pointer approach
+        int l=0,r=0,profit=0,currprofit=0;
+        while(r<prices.length)
+        {
+            //if buy>sell, move pinters by one index
+            //place left at right(next position), move right by one
+            if(prices[l]>prices[r])
+            {
+              l=r;
+              r++;
+            }
+            //if sell>buy
+            else
+            {
+                currprofit=prices[r]-prices[l];
+                profit=Math.max(profit, currprofit);
+                r++;
+            }
+        }
+        return profit;
+
+    }
+}
