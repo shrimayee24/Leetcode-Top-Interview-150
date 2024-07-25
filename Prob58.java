eb146ca0a5c8f0a67aab6cd6a47989b6299fb9be
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-07-25 13:28:02 +0530

    Create Prob58.java

---
+++ Prob58.java
@@ -0,0 +1,23 @@
+class Prob58 {
+    public int lengthOfLastWord(String s) 
+    {
+        //remove whitespaces at beginning of first string and end of last string
+        String str=s.trim();
+        int count=0;
+
+        //iterate from last string, increment count
+        //if whitespace occurs, break loop
+        for(int i=str.length()-1; i>=0; i--)
+        {
+            if(str.charAt(i)==' ')
+            {
+                break;
+            }
+            else
+            {
+                count++;
+            }
+        }  
+        return count;      
+    }
+}
