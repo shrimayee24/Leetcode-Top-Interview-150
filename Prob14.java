5a0eb9362d587f79176064ab836f94be085d8ada
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-07-25 13:25:57 +0530

    Create Prob14.java

---
+++ Prob14.java
@@ -0,0 +1,31 @@
+class Prob14 {
+    public String longestCommonPrefix(String[] strs) 
+    {
+        //sort in alphabetical order
+        Arrays.sort(strs);
+        
+        //common prefix between first and last string exists, same prefix exists bet      middle strings too
+     String first=strs[0];
+        String last=strs[strs.length-1];
+        int index=0;
+        
+        //str[0] has least length-only iterate till length of first string.
+        while(index<first.length())
+        {
+            if(first.charAt(index)==last.charAt(index))
+            {
+                index++;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+    
+    //return substring matched till index iterated
+    return first.substring(0,index);
+
+
+    }
+}
