2a7ad6b0f76f6237134b63597148a861a3c1d5a0
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-08-02 22:01:02 +0530

    Update Prob26.java

--- Prob26.java
+++ Prob26.java
@@ -1,10 +1,13 @@
+//remove duplicate elements from sorted array
 class Prob26
 {
     public int removeDuplicates(int[] nums) 
     {
        int count=1;
+	    
 		for(int i=1;i<nums.length; i++)
 		{
+			//if previous element is not equal to next element, both are unique elements, increment counter
 			if(nums[i]!=nums[i-1])
 			{
 				nums[count]=nums[i];
