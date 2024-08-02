51af1c37240d6fdf2d37ed22a261d474ed9d2d81
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-08-02 22:03:16 +0530

    Update Prob27.java

--- Prob27.java
+++ Prob27.java
@@ -1,9 +1,12 @@
+//almost same as problem 26
+//remove element mentioned in an integer array
 class Prob27  {
     public int removeElement(int[] nums, int val) 
     {
         int count=0, i=0;
         for(i=0;i<nums.length;i++)
         {
+            //if element to be removed exists, dont increment counter at that index iteration
             if(nums[i]!=val)
             {
                 nums[count]=nums[i];
