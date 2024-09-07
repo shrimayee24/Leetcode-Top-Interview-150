b21c75d7bc92a1418f694748dd03171969b1d57d
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-09-07 12:16:07 +0530

    Create Prob141.java

---
+++ Prob141.java
@@ -0,0 +1,27 @@
+//detect cycle in linked list
+ 
+//slow pointer fast pointer approach
+ //if two meet- cycle exists
+
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {
+        ListNode slow=head, fast=head;
+        //at starting both pointers are at head
+        while(fast!=null && fast.next!=null)
+        //fast is at some object and next pointer shouldnt point to null
+        {
+            slow=slow.next;
+            //traverse slow by 1 step at a time
+            fast=fast.next.next;
+            //traverse fast by 2 steps at a time
+            if(slow==fast)
+            //if both are at same object, cycle exists
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
