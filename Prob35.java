9436911b43d32d43811088a335d0e6f0143ae2e3
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-09-08 16:39:56 +0530

    Create Prob35.java

---
+++ Prob35.java
@@ -0,0 +1,29 @@
+//to search a given element in an array
+//if element not found, insert element at correct position of ascending order.
+
+class Prob35
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+       for(int i=0; i<nums.length; i++)
+       {
+         //if target found, return index, dont check further
+          if(nums[i]==target)
+          {
+            return i;
+          }
+          
+          //if target number is less than curr number, we would have to shift currnumber by 1 and place target at the position of currnumber.
+          //we would have to return index of currnumber which is i
+          if(nums[i]>target)
+          {
+            return i;
+          }
+        }
+        
+        //if target is greater than currnumber, there must be some number ahead which   is greater than target
+          //if no number ahead left which is >target then target is greatest of all elements, placed at the index next to last index=length of array.
+          return nums.length;
+       
+    }
+}
