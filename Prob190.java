1cc4900563c2fafac548e6c5662a396ffb21ed7d
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-09-10 00:44:23 +0530

    Create Prob190.java

---
+++ Prob190.java
@@ -0,0 +1,12 @@
+//reverse bits of binary representaion of a number
+//In java, inbuilt function Integer.reverse() which reverses the binary digits of a decimal
+//returns decimal representation of reversed bits
+
+public class Prob190
+{
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) 
+    {
+       return Integer.reverse(n);
+    }
+}
