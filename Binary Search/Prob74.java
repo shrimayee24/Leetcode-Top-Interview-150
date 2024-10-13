dababba775149f873d1a3db5162d6dddd04304b7
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-13 16:14:13 +0530

    Create Prob74.java

---
+++ Binary Search/Prob74.java
@@ -0,0 +1,37 @@
+//search for element
+//locate row of element first, then search by column
+class Prob74 
+{
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+        int row=0,difference=Integer.MAX_VALUE,currdifference=0;
+        
+        //locate row
+        for(int i=0;i<matrix.length;i++)
+        {
+             //find row where first element is less than target
+            if(matrix[i][0]> target) continue;
+            //calculate minimum difference between element and target
+             currdifference=Math.min(difference,target-matrix[i][0]);
+             //if difference updated,update row
+                if(currdifference!=difference)
+                {
+                    row=i;
+                }
+                //update previous value for next iteration
+                difference=currdifference;
+            
+        }
+        //after finding row,search columnwise for that particular row only
+        for(int j=0;j<matrix[0].length;j++)
+        {
+            if(target==matrix[row][j])
+            {
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+}
