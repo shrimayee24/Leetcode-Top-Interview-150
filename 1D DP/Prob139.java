752019ecf3956ec72804e650c7cd13b2795adb6c
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-11-26 02:00:31 +0530

    Update Prob139.java

--- 1D DP/Prob139.java
+++ 1D DP/Prob139.java
@@ -10,22 +10,26 @@ class Prob139
         // DP array to track if the string up to index i can be segmented
         //index of dp array: end of a substring:substring contains characters till i-1th index
         boolean[] dp = new boolean[n + 1];
+        
         dp[0] = true; // Base case: till 0th index,there are no characters- empty string can always be segmented
 
 //i:substring till i-1th index
 //j:start of substring
-//iteratively find substrings keeping end constant(outer loop) and incrementing start at each iteration(inner loop)
+
+        //iteratively find substrings keeping end constant(outer loop) and incrementing start at each iteration(inner loop)
         for (int i = 1; i <= n; i++) 
         {
             for (int j = 0; j < i; j++) {
                 // Check if substring s[j:i] exists in the wordSet
                 String currstr=s.substring(j, i);
-                //dp[j]: previous section from 0 to j-1th index is a valid string in the list or not.
+                
+                //dp[j]: previous section till j-1th index is a valid string in the list or not. (start of previous section doesn't matter)
                 if (dp[j] && wordSet.contains(currstr)) 
                 {
                     //start index of currstring (j)=end index of previous sections for next iterations (i)
                     dp[i] = true;
                     break; // No need to check further if dp[i] is true
+                    
                     //dont shrink the substrings further till i-1th index
                 }
             }
