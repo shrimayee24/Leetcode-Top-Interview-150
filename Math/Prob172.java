348092cc879fb63676e4e030c054d774056d274b
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-11-27 12:17:04 +0530

    Update Prob172.java

--- Math/Prob172.java
+++ Math/Prob172.java
@@ -1,12 +1,14 @@
-//count number of trailing zeros in factorial of a number
+//count number of trailing zeros
 //iteratively divide number by 5 till we get 1
 //number of trailing zeros=addition of all quotients of each iteration
 class Solution 
 {
     public int trailingZeroes(int n) 
     {
-        int count = 0;
+        //if number is less than 5, cannot be divided by 5
+        if(n<5) return 0;
         
+        int count = 0;
         //iteratively divide by 5
         while (n >= 5) 
         {
