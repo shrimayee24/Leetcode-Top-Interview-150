ad93ee367dee452007b913d0a7ed3e9d560e8a55
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2025-10-24 15:42:50 +0530

    Update Prob42.java

--- Array+String/Prob42.java
+++ Array+String/Prob42.java
@@ -1,62 +1,46 @@
-//calculate quantity of rainwater trapped
-//bruteforce approach
-class Prob42
+//Dynamic Programming (prefix-max & suffix-max arrays) approach
+class Prob42 
 {
     public int trap(int[] height) 
     {
-        int totalwater=0;;
-        for(int i=0;i<height.length;i++)
+        int len=height.length;
+        //find max heights of bars on both sides
+
+        //array to calculate max height on the left of each bar 
+     //start from left side
+        //traverse left to right, update max height on each iteration
+        int left[]=new int[len];
+
+        //initialize leftmost index to leftmost bar
+left[0]=height[0];
+        for(int i=1;i<len;i++)
         {
-            //find maximum height at left
-            int maxleft=maxleft(i,height);
-            
-            //find maximum right at right
-            int maxright=maxright(i,height);
+            //update index to the max of the current height (height[i]) and the height calculated till now (left[i-1])
+            left[i]=Math.max(height[i],left[i-1]);
+        }
 
-            //find minimum of maximum heights found at left and right
-            int minwater=Math.min(maxleft,maxright);
-            
-            //water stored on top of current bar would be minimum quantity that can be stored- space occupied by height of current bar
-            int currwater=minwater-height[i];
+//array to calculate max height on the right of each bar 
+     //start from right side
+        //traverse right to left, update max height on each iteration
+        int right[]=new int[len];
 
-            //if water can not be stored,dont add it to total quantity
-            if(currwater>0)
-            {
-                totalwater+=currwater;
-            }
-            
+        //initialize rightmost index to rightmost bar
+right[len-1]=height[len-1];
+        for(int i=len-2;i>=0;i--)
+        {
+            //update index to the max of the current height (height[i]) and the height calculated till now (left[i-1])
+            right[i]=Math.max(height[i],right[i+1]);
+        }
+
+        //calculate water amount between each 2 bars
+        int amount=0;
+        for(int i=0;i<len;i++)
+        {
+            //take min of heights of max heights of bars at left and right w.r.t current bar
+            //amount=min-curr height
+            amount+=Math.min(left[i],right[i])-height[i];
 
         }
-        return totalwater;
-        
-        
+return amount;
     }
-    //find maximum height on left side of current bar
-    public int maxleft(int index,int[] height)
-    {
-        int maxleft=0;
-        //iterate through all bars previous to current bar
-        for(int i=0;i<index;i++)
-        {
-            //find maximum height at each iteration
-            maxleft=Math.max(maxleft,height[i]);
-            //use this height to compare next element
-        }
-        return maxleft;
-    }
-     //find maximum height on right side of current bar
-    public int maxright(int index,int[] height)
-    {
-        int maxright=0;
-        //iterate through all bars previous to current bar
-            for(int i=index+1;i<height.length;i++)
-            {
-                //find maximum height at each iteration
-                maxright=Math.max(maxright,height[i]);
-                //use this height to compare next element
-
-            }
-            return maxright;
-    }
-
 }
